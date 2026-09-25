0985dee9dd4f6f733896b18d49f01828acbfa492
sumittkumarr11 <sumitkrr0110@gmail.com>
2026-09-25 22:14:39 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://node-js-technical-test-by-gurmeet-s.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
